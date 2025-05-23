3cb575d01d8bb6c467f8111827e1d5de46017086
Navadeep Bommarapu <153001918+Navadeep-bommarapu@users.noreply.github.com>
2025-05-23 09:28:17 +0530

    Time: 2 ms (6.83%), Space: 42.6 MB (15.37%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,9 +1,13 @@
 import java.util.Arrays;
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i = 0; i < n; i++){
-            nums1[m+i] = nums2[i];
+        int left = m, right = 0;
+        while(left < nums1.length){
+            if(right < n){
+                nums1[left++] = nums2[right++];
+            }
         }
         Arrays.sort(nums1);
+        System.out.print(Arrays.toString(nums1));
     }
 }
