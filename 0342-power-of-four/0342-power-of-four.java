d3d8e4254270c48aa0c83576984410ed379ecdee
Navadeep Bommarapu <153001918+Navadeep-bommarapu@users.noreply.github.com>
2025-05-27 08:20:29 +0530

    Time: 0 ms (100%), Space: 40.9 MB (46.66%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        for(int i = 0; i<=31 ; i++){
-            if(Math.pow(4,i) == n ){
-                return true;
-            }
+        if(n == 0) return false;
+        if(n == 1) return true;
+
+        while(n != 1){
+            if(n % 4 != 0) return false;
+            n = n / 4;
         }
         
-        return false;
+        return true;
     }
 }
